3f6bc8c8033be2bfbc9d925fa4697c0fbf7761cd
AVEV <rravikumar34@gmail.com>
2022-09-13 19:23:03 +0530

    Day-02 Updated

---
+++ Day-02/AVEV/demo.py
@@ -0,0 +1,13 @@
+a = 10
+
+def dg(p,q):
+    return p+q
+
+class Student:
+    def stdinfo():
+        print("Student Record:")
+        return
+   
+    def marks():
+        print("Studnet Marks:")
+        return
